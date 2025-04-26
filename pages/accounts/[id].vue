afdc3535eb3c04a9fdc2f6c0ecc06d3c71dca864
Eldos <eldos.baktybekov@gmail.com>
2025-04-26 13:49:27 +0600

    fix: import MainButton in account detail page for consistency

--- pages/accounts/[id].vue
+++ pages/accounts/[id].vue
@@ -2,17 +2,21 @@
   <div>
     {{ account }}
     <SettingsButton />
-    <MainButton text="Добавить запись" :has-shine-effect="true" />
     <BackButton @click="navigateTo({ name: 'index' })" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { SettingsButton, BackButton, MainButton } from "vue-tg"
+import { SettingsButton, BackButton, useMainButton } from "vue-tg"
 
 const route = useRoute()
+
 
 const accountId = Number(route.params.id as string)
 
 const { data: account } = await useAccountById(accountId)
+
+const { show } = useMainButton()
+
+onMounted(show)
 </script>
